a3f5790a4c1b44af6459b0f1ffa0de2c1d69a92e
DarkDev <darkdev_brand@riseup.net>
2023-11-03 19:37:06 +0200

    refactoring text block

--- src/lib/widgets/ui/landing/About.svelte
+++ src/lib/widgets/ui/landing/About.svelte
@@ -1,52 +1,63 @@
+<script>
+	const aboutUsText = [
+		{
+			title: 'Lorem ipsum is placeholder',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			icon: ''
+		},
+		{
+			title: 'Lorem ipsum is placeholder',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			icon: ''
+		},
+		{
+			title: 'Lorem ipsum is placeholder',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			icon: ''
+		},
+		{
+			title: 'Lorem ipsum is placeholder',
+			desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			icon: ''
+		}
+	];
+</script>
+
 <section>
+	<a name="about" />
+
 	<div class=" mx-auto flex max-w-7xl flex-col items-center px-5 py-8 sm:px-6 lg:px-8">
-		<div class="prose-blue prose mx-auto flex w-full max-w-3xl flex-col text-left">
-			<div class="mx-auto w-full">
-				<h1>Lorem ipsum is placeholder text commonly used.</h1>
-				<h2>Lorem ipsum is placeholder text commonly used.</h2>
-				<p>
+		<div class="prose-primary prose mx-auto flex w-full max-w-3xl flex-col text-left">
+			<div class="mx-auto w-full ">
+				<h1 class="text-primary">Lorem ipsum is placeholder text commonly used.</h1>
+				<h2 class="text-violet-800">Lorem ipsum is placeholder text commonly used.</h2>
+				<p class="text-violet-800">
 					Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
 					industries for previewing layouts and visual mockups.
 				</p>
 			</div>
 		</div>
-		<a name="about" />
 
 		<div
-			class="mx-auto my-10 flex max-w-7xl flex-col items-center border-b border-gray-200 pb-10 sm:flex-row"
+			class="mx-auto my-10 flex max-w-7xl flex-col items-center  pb-10 "
 		>
-			<div
-				class="mx-auto my-10 flex max-w-7xl flex-col items-center border-b border-gray-200 pb-10 sm:flex-row"
-			>
+			{#each aboutUsText as text}
 				<div
-					class="inline-flex h-20 w-20 flex-shrink-0 items-center justify-center rounded-full bg-gray-50 text-neutral-600 sm:mr-10"
+					class="mx-auto my-10 flex max-w-7xl flex-col items-center border-b border-primary pb-10 sm:flex-row"
 				>
-					<svg
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="1.5"
-						class="h-10 w-10"
-						viewBox="0 0 24 24"
+					<div
+						class="inline-flex h-20 w-20 flex-shrink-0 items-center justify-center rounded-full bg-gray-50 text-primary sm:mr-10"
 					>
-						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-						<circle cx="12" cy="12" r="9" />
-						<line x1="3.6" y1="15" x2="14.15" y2="15" />
-						<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(72 12 12)" />
-						<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(144 12 12)" />
-						<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(216 12 12)" />
-						<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(288 12 12)" />
-					</svg>
+						<img src={text.icon} class='' alt="" />
+					</div>
+					<div class="prose-md prose mt-6 flex-grow text-center sm:mt-0 sm:text-left">
+						<h2 class='text-violet-800'>{text.title}</h2>
+						<p class='text-violet-800'>
+							{text.desc}
+						</p>
+					</div>
 				</div>
-				<div class="prose-md prose mt-6 flex-grow text-center sm:mt-0 sm:text-left">
-					<h2>Short length headline.</h2>
-					<p>
-						Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
-						industries for previewing layouts and visual mockups.
-					</p>
-				</div>
-			</div>
+			{/each}
 		</div>
 	</div>
 </section>
