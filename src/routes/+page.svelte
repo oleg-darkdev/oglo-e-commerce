aa8636b8ad4925eee0096e9f1327fbc7fa8a187c
DarkDev <darkdev_brand@riseup.net>
2023-11-03 18:21:34 +0200

    base structure of landing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Faq, Features, Hero, About, Pricing, Contact, Community, Footer } from '../lib/widgets/';
+</script>
+
+<!-- hero  -->
+<Hero />
+
+<About />
+
+<!-- community - github links and other things  -->
+<Community />
+
+
+<Features />
+
+<Pricing />
+
+<!-- contact -->
+<Contact />
+
+
+<!-- faq -->
+<Faq />
+
+<!-- footer -->
+
+<Footer />
