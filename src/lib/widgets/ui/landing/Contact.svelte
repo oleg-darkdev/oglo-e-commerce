4ac72bd6e6e5207732d2955bfae0b8253adfe4b0
DarkDev <darkdev_brand@riseup.net>
2023-11-03 18:22:24 +0200

    alfa v., add anchore

---
+++ src/lib/widgets/ui/landing/Contact.svelte
@@ -0,0 +1,65 @@
+<section class="z-0 bg-primary">
+	<div class="container mx-auto grid gap-8 px-6 py-12 lg:grid-cols-3">
+		<div>
+			<p class="font-medium text-secondary  ">Skontaktuj się z nami</p>
+
+			<h2
+				class="max-w-3xl text-2xl  font-bold leading-none tracking-tighter text-neutral-200 md:text-5xl lg:max-w-5xl lg:text-4xl"
+			>
+				Chcielibyśmy poznać Twoje zdanie
+			</h2>
+
+			<p class="mt-3 text-gray-400">Nasz przyjazny zespół jest zawsze gotowy do rozmowy.</p>
+		</div>
+
+		<a name="contact" />
+
+		<div class="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:col-span-2 ">
+			<div class="rounded-lg bg-[#330066] p-4 md:p-6">
+				<span class="inline-block rounded-lg bg-secondary p-3 text-primary">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+						/>
+					</svg>
+				</span>
+
+				<h2 class="mt-4 text-base font-medium text-white">Czat do działu sprzedaży</h2>
+				<p class="mt-2 text-sm text-gray-400">Porozmawiaj z naszym zespołem.</p>
+				<p class="mt-2 text-sm text-secondary">darkdev_brand@riseup.net</p>
+			</div>
+
+			<div class="rounded-lg bg-[#330066] p-4 md:p-6">
+				<span class="inline-block rounded-lg bg-secondary p-3 text-primary">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+						/>
+					</svg>
+				</span>
+
+				<h2 class="mt-4 text-base font-medium text-white">Zadzwoń do nas</h2>
+				<p class="mt-2 text-sm text-gray-400">Od poniedziałku do piątku od 8:00 do 17:00.</p>
+				<p class="mt-2 text-sm text-secondary">+48 662 290 677</p>
+			</div>
+		</div>
+	</div>
+</section>
