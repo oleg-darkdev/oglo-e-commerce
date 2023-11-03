f27b8548a81d6fc6e6d339e395427871a795e9ac
DarkDev <darkdev_brand@riseup.net>
2023-11-03 18:22:28 +0200

    alfa v., add anchore

---
+++ src/lib/widgets/ui/landing/Community.svelte
@@ -0,0 +1,36 @@
+<section class="mb-12 mt-6 flex w-full justify-center">
+	<div class="max-w-2xl  overflow-hidden rounded-lg  bg-gray-800 shadow-md">
+		<img class="h-64 w-full object-cover" src="" alt="Community image" />
+
+		<a name="сommunity" />
+
+		<div class="p-6">
+			<div>
+				<span class="text-xs font-medium uppercase text-secondary">O społecznośći</span>
+				<a
+					href="#"
+					class="mt-2 block transform text-xl font-semibold text-gray-800 transition-colors duration-300 hover:text-gray-600 hover:underline dark:text-white"
+					tabindex="0"
+					role="link">Lorem ipsum dolor sit amet, consectetur.</a
+				>
+				<p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum
+					volutpat vel. Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec
+					senectus erat. Et malesuada lobortis.
+				</p>
+			</div>
+
+			<div class="mt-4">
+				<div class="flex items-center">
+					<div class="flex items-center">
+						<img class="h-10 rounded-full object-cover" src="" alt="Avatar" />
+						<a href="#" class="mx-2 font-semibold text-gray-200" tabindex="0" role="link"
+							>Oleg Medvedev</a
+						>
+					</div>
+					<span class="mx-1 text-xs text-gray-600 dark:text-gray-300">3 Listopada 2023</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
