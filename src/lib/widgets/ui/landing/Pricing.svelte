33a9300b463e49f4c292d165dee1d4d607a4d1a5
DarkDev <darkdev_brand@riseup.net>
2023-11-06 14:33:36 +0200

    upd text, refactoring layout

--- src/lib/widgets/ui/landing/Pricing.svelte
+++ src/lib/widgets/ui/landing/Pricing.svelte
@@ -1,136 +1,222 @@
-<section class='ml-20 relative bg-[#330066] w-8/12 rounded-xl -my-8 z-30'>
-  <div class="relative items-center w-full py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
-    <div class="flex flex-wrap items-start justify-center w-full gap-4 px-5 mx-auto lg:px-0">
-      <div class="w-full xl:w-1/3 lg:w-2/6 md:text-center lg:text-left">
-        <a name="price"></a>
-        <div class="flex flex-col p-8 lg:p-0">
-          <strong class="mb-1 text-xs font-semibold tracking-widest text-secondary uppercase"> </strong>
-          <span class="mb-8 text-2xl font-bold text-neutral-300 tracking-relaxed lg:text-5xl"> Pricing </span>
-          <p class="mx-auto mb-4 text-xl font-light text-gray-500 text-opacity-70 lg:pr-10">Use the free templates with your whole team or choose a premium.</p>
-          <p class="mx-auto mb-2 text-xl font-light text-gray-500 text-opacity-70 lg:pr-10">Get the bundle and get lifetime support and one year updates.</p>
+<script>
+	const microPlan = [
+		{
+			text: 'Jedno zlecenie',
+			price: '$0.30'
+		},
+		{
+			text: 'Pozycje katalogu',
+			price: '$0.03'
+		},
+		{
+			text: '1 GB danych',
+			price: '$0.30'
+		}
+		// {
+		//   text: 'API połączeńie:',
+		//   price: '$0,00012'
+		// },
+	];
 
+	const macroPlan = [
+		{
+			text: 'Jedno zlecenie',
+			price: '$0.15'
+		},
+		{
+			text: 'Pozycje katalogu',
+			price: '$0.015'
+		},
+		{
+			text: '1 GB danych',
+			price: '$0.20'
+		}
+		// {
+		//   text: 'API połączeńie:',
+		//   price: '$0,00012'
+		// },
+	];
 
-        </div>
-      </div>
-      <div class="w-full xl:w-1/4 md:w-2/6">
-        <div class="relative flex flex-col h-full p-8 bg-gray-50 rounded-xl hover:border-white">
-          <span class="mb-4 text-base font-medium tracking-widest text-neutral-600 uppercase"> Starter </span>
-          <span class="flex items-center mb-8 text-base font-medium tracking-tight text-neutral-600"> Try and decide. </span>
-          <div class="flex items-end text-3xl font-black leading-none text-neutral-600 lg:text-4xl">
-            <span>$0 </span>
-          </div>
-          <ul>
-            <li class="flex items-center mt-8 mb-2 text-base font-medium tracking-tight text-neutral-600">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-neutral-600">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>2 demo templates.</strong>
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-neutral-600">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-neutral-600">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>Fully responsive.</strong>
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-neutral-600 text-opacity-70">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-neutral-600 text-opacity-70">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
-              </span>
-              Figma.
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-neutral-600 text-opacity-70">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-neutral-600 text-opacity-70">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
-              </span>
-              Lifetime Support.
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-neutral-600 text-opacity-70">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-neutral-600 text-opacity-70">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg> </span>Free Updates
-            </li>
-          </ul>
+	const basicInclude = ['Nielimitowani użytkownicy', 'Wszystkie języki'];
+</script>
 
-          <!-- <div class="mt-8">
+<section class="relative z-30 -my-8 ml-20 w-8/12 rounded-xl bg-[#330066]">
+	<div class="relative mx-auto w-full max-w-7xl items-center py-24 md:px-12 lg:px-16">
+		<div class="mx-auto flex w-full flex-wrap items-start justify-center gap-4 px-5 lg:px-0">
+			<div class="w-full md:text-center lg:w-2/6 lg:text-left xl:w-1/3">
+				<a name="price" />
+				<div class="flex flex-col p-8 lg:p-0">
+					<strong class="mb-1 text-xs font-semibold uppercase tracking-widest text-secondary" />
+					<span class="tracking-relaxed mb-8 text-2xl font-bold text-neutral-300 lg:text-4xl">
+						Nasze ceny są idealne dla mikro i małych przedsiębiorców.
+					</span>
+					<p class="mx-auto mb-4 text-xl font-light text-gray-500 text-opacity-70 lg:pr-10">
+						Oferujemy przystępne i elastyczne ceny, które pomogą Ci osiągnąć cele bez nadmiernych
+						kosztów.
+					</p>
+					<p class="mx-auto mb-2 text-xl font-light text-gray-500 text-opacity-70 lg:pr-10">
+						Dokonuj sprzedaży każdego miesiąca - bez limitów i bez obowiązkowej opłaty
+						abonamentowej.
+					</p>
+				</div>
+			</div>
+			<div class="w-full md:w-2/6 xl:w-1/4">
+				<div class="relative flex h-full flex-col rounded-xl bg-gray-50 p-8 hover:border-white">
+					<span class="mb-4 text-base font-medium uppercase tracking-widest text-violet-900">
+						Micro
+					</span>
+					<span class="mb-8 flex items-center text-base font-medium tracking-tight text-violet-900">
+						Dla mikroprzedsiębiorców
+					</span>
+					<div class="flex  items-end text-3xl font-black leading-none text-violet-900 lg:text-4xl">
+						<span class="text-violet-900">$0 </span>
+						<span class="ml-4 font-light text-violet-800">/ FREE </span>
+					</div>
+					<ul class="mt-8">
+						{#each basicInclude as basic}
+							<li
+								class=" my-3 flex items-center text-base font-medium tracking-tight text-violet-900"
+							>
+								<span
+									class="mr-2 inline-flex h-5 w-5 flex-shrink-0 items-center justify-center text-violet-900"
+								>
+									<svg
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2.5"
+										class="h-3 w-3"
+										viewBox="0 0 24 24"
+									>
+										<path d="M20 6L9 17l-5-5" />
+									</svg>
+								</span>
+								<strong>{basic}</strong>
+							</li>
+						{/each}
+
+						<li
+							class="mb-2 flex items-center text-base font-medium tracking-tight text-violet-900 text-opacity-70"
+						>
+							<span
+								class="mr-2 inline-flex h-5 w-5 flex-shrink-0 items-center justify-center text-violet-900 text-opacity-70"
+							>
+								<svg
+									fill="none"
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2.5"
+									class="h-3 w-3"
+									viewBox="0 0 24 24"
+								>
+									<line x1="18" y1="6" x2="6" y2="18" />
+									<line x1="6" y1="6" x2="18" y2="18" />
+								</svg>
+							</span>
+							Lifetime Support.
+						</li>
+						{#each microPlan as price}
+							<li
+								class="between my-3 flex flex-wrap items-center text-base font-light tracking-tight text-violet-900"
+							>
+								<strong class="max-w-[40px]">{price.text}</strong>
+								<span class="ml-1 font-bold">{price.price}</span>
+							</li>
+						{/each}
+					</ul>
+
+					<!-- <div class="mt-8">
             <button class="w-full items-center block px-10 py-3.5 text-base font-medium text-center text-blue-600 transition duration-500 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 bg-white">Download</button>
           </div> -->
-        </div>
-      </div>
-      <div class="w-full xl:w-1/4 md:w-2/6">
-        <div class="relative flex flex-col h-full p-8 bg-secondary rounded-xl hover:border-white">
-          <span class="mb-4 text-base font-medium tracking-widest text-white uppercase"> Bundle </span>
-          <span class="flex items-center mb-8 text-base font-medium tracking-tight text-white"> No need to subscribe </span>
-          <div class="flex items-end text-3xl font-black leading-none text-white lg:text-4xl">
-            <span>
-              <span>$69</span>
-              <del class="text-2xl font-normal text-white">$149</del>
-            </span>
-          </div>
-          <ul>
-            <li class="flex items-center mt-8 mb-2 text-base font-medium tracking-tight text-white">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-white">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>All templates.</strong>
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-white">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-white">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>Figma files.</strong>
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-white">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-white">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>Free Updates.</strong>
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-white">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-white">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>Fully Responsive</strong>
-            </li>
-            <li class="flex items-center mb-2 text-base font-medium tracking-tight text-white">
-              <span class="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 mr-2 text-white">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                  <path d="M20 6L9 17l-5-5"></path>
-                </svg>
-              </span>
-              <strong>Lifetime Support</strong>
-            </li>
-          </ul>
+				</div>
+			</div>
+			<div class="w-full md:w-2/6 xl:w-1/4">
+				<div class="relative flex h-full flex-col rounded-xl bg-secondary p-8 hover:border-white">
+					<span class="mb-4 text-base font-medium uppercase tracking-widest text-white">
+						Macro
+					</span>
+					<span class="mb-8 flex items-center text-base font-medium tracking-tight text-white">
+						Dla małych przedsiębiorców.
+					</span>
+					<div class="flex items-end text-3xl font-black leading-none text-white lg:text-4xl">
+						<span>
+							<span>$8</span>
+							<del class="text-2xl font-normal text-white">$16</del>
+						</span>
+					</div>
+					<ul class='mt-8'>
+						{#each basicInclude as basic}
+							<li
+								class=" mb-2 flex items-center text-base font-medium tracking-tight text-white"
+							>
+								<span
+									class="mr-2 inline-flex h-5 w-5 flex-shrink-0 items-center justify-center text-white"
+								>
+									<svg
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2.5"
+										class="h-3 w-3"
+										viewBox="0 0 24 24"
+									>
+										<path d="M20 6L9 17l-5-5" />
+									</svg>
+								</span>
+								<strong>{basic}</strong>
+							</li>
+						{/each}
 
-          <!-- <div class="mt-8">
+						<li class="mb-2 flex items-center text-base font-medium tracking-tight text-white">
+							<span
+								class="mr-2 inline-flex h-5 w-5 flex-shrink-0 items-center justify-center text-white"
+							>
+								<svg
+									fill="none"
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2.5"
+									class="h-3 w-3"
+									viewBox="0 0 24 24"
+								>
+									<path d="M20 6L9 17l-5-5" />
+								</svg>
+							</span>
+							<strong>Lifetime Support</strong>
+						</li>
+						{#each macroPlan as price}
+							<li
+								class="between my-3 flex flex-wrap items-center text-base font-light tracking-tight text-white"
+							>
+								<strong class="max-w-[40px]">{price.text}</strong>
+								<span class="ml-1 font-bold">{price.price}</span>
+							</li>
+						{/each}
+					</ul>
+
+					<!-- <div class="mt-8">
             <button class="w-full items-center block px-10 py-3.5 text-base font-medium text-center text-blue-600 transition duration-500 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 bg-white">Buy for $69</button>
           </div> -->
-        </div>
-      </div>
-    </div>
+				</div>
+			</div>
+		</div>
 
-
-    <div class='w-10/12 mx-auto mt-8'>
-    <img src="./images/social.svg" alt="" class="h-[300px] w-full rounded-xl border-secondary border-2">
-  </div>
-  </div>
+		<div class="mx-auto mt-8 w-10/12">
+			<img
+				src="./images/social.svg"
+				alt=""
+				class="h-[300px] w-full rounded-xl border-2 border-secondary"
+			/>
+		</div>
+	</div>
 </section>
+
+<style>
+	.between {
+		justify-content: space-between;
+	}
+</style>
